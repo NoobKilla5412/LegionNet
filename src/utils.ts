1fc2125f3e7fade58854fa322af14ecb97c6a1ed
Trifive <147096352+Trifive@users.noreply.github.com>
2025-09-23 23:33:40 -0700

    Edited some stuff
    Deleted some empty lines and comments

--- src/utils.ts
+++ src/utils.ts
@@ -77,4 +77,4 @@ export function createObfuscatedWindow(url: string) {
 
 export const parser = new DOMParser();
 
-export const password = "SDIYBT";
+export const password = "67mustardmangolabubu";
